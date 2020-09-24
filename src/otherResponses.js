9865826315d6b7cda3b9d71dfe51668adbbc3d3c
Brady Friese <bmf4295@g.rit.edu>
2020-09-24 18:24:43 -0400

    =Got base functionality of POST and GET request added, will work on DELETE request next

---
+++ src/otherResponses.js
@@ -0,0 +1,88 @@
+const users = {};
+
+const respondJSON = (req, res, content, statusCode) => {
+  const headers = {
+    'Content-Type': 'application/json',
+  };
+  res.writeHead(statusCode, headers);
+  res.write(JSON.stringify(content));
+  res.end();
+};
+
+const respondJSONMeta = (req, res, statusCode) => {
+  const headers = {
+    'Content-Type': 'application/json',
+  };
+  res.writeHead(statusCode, headers);
+  res.end();
+};
+
+const getUsers = (req, res, query) => {
+  let responseJSON;
+  if (!query) {
+    responseJSON = {
+      users,
+    };
+  } else {
+    responseJSON = users[query.name];
+  }
+
+  return respondJSON(req, res, responseJSON, 200);
+};
+
+const getUsersMeta = (req, res) => respondJSONMeta(req, res, 200);
+
+const notReal = (req, res) => {
+  const responseJSON = {
+    message: 'The page you are looking for was not able to be found',
+    id: 'notFound',
+  };
+  return respondJSON(req, res, responseJSON, 404);
+};
+
+const notRealMeta = (req, res) => respondJSONMeta(req, res, 404);
+
+const updateUsers = (req, res, body) => {
+  const responseJSON = {
+    message: 'Name, Title, Type and Status are all required.',
+  };
+  let responseCode = 201;
+
+  if (!body.name || !body.title || !body.type || !body.status) {
+    responseJSON.id = 'missingParams';
+    return respondJSON(req, res, responseJSON, 400);
+  }
+
+  if (users[body.name]) {
+    responseCode = 204;
+  } else {
+    users[body.name] = { Movies: [], Series: [] };
+  }
+  const { title, status, type } = body;
+  if (type === 'Movie') {
+    users[body.name].Movies.push({
+      Title: title,
+      Status: status,
+    });
+  } else {
+    users[body.name].Series.push({
+      Title: title,
+      Status: status,
+    });
+  }
+
+  if (responseCode === 201) {
+    responseJSON.message = 'Created Successfully';
+    return respondJSON(req, res, responseJSON, responseCode);
+  }
+
+  return respondJSONMeta(req, res, responseCode);
+};
+// exports
+module.exports = {
+  getUsers,
+  getUsersMeta,
+  notReal,
+  notRealMeta,
+  updateUsers,
+};
---
+++ src/server.js
@@ -0,0 +1,68 @@
+const http = require('http');
+const url = require('url');
+const query = require('querystring');
+const htmlHandler = require('./htmlResponses');
+const otherHandler = require('./otherResponses');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const urlStruct = {
+  GET: {
+    '/': htmlHandler.getIndex,
+    '/style.css': htmlHandler.getIndexCSS,
+    '/getUsers': otherHandler.getUsers,
+    '/notReal': otherHandler.notReal,
+    notFound: otherHandler.notReal,
+
+  },
+  HEAD: {
+    '/getUsers': otherHandler.getUsersMeta,
+    '/notReal': otherHandler.notRealMeta,
+    notFound: otherHandler.notRealMeta,
+  },
+  POST: {
+    '/addTitle': otherHandler.updateUsers,
+  },
+};
+const handlePost = (req, res, parsedUrl) => {
+  if (parsedUrl.pathname === '/addTitle') {
+    const body = [];
+
+    req.on('error', (err) => {
+      console.log(err);
+      res.statusCode = 400;
+      res.end();
+    });
+    req.on('data', (chunk) => {
+      body.push(chunk);
+    });
+    req.on('end', () => {
+      const bodyString = Buffer.concat(body).toString();
+
+      const bodyParams = query.parse(bodyString);
+
+      urlStruct.POST[parsedUrl.pathname](req, res, bodyParams);
+    });
+  }
+};
+const handleGet = (req, res, parsedUrl, method, queryParams) => {
+  if (urlStruct[method][parsedUrl.pathname]) {
+    urlStruct[method][parsedUrl.pathname](req, res, queryParams);
+  } else {
+    urlStruct[method].notFound(req, res);
+  }
+};
+
+const onRequest = (req, res) => {
+  const parsedUrl = url.parse(req.url);
+  const params = query.parse(parsedUrl.query);
+  const { method } = req;
+  if (method === 'POST') {
+    handlePost(req, res, parsedUrl);
+  } else {
+    handleGet(req, res, parsedUrl, method, params);
+  }
+};
+http.createServer(onRequest).listen(port);
+
+console.log(`Listening on localhost:${3000}`);
